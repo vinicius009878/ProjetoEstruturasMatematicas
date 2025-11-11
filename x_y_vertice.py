c68f7f19a493153416c63bb3f1dbab4db7bb1b85
Vinicius Antonino <gabrielantonino2004@gmail.com>
2025-11-11 17:28:25 -0300

    :sparkles: feat: Adição da função que calcula X e Y do vertice isolada

---
+++ x_y_vertice.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sympy as sp
+
+def calcular_vertice():
+    a = float(input("Digite o valor de a: "))
+    b = float(input("Digite o valor de b: "))
+    c = float(input("Digite o valor de c: "))
+    xv = -b / (2*a)
+    yv = a*xv**2 + b*xv + c
+    print(f"Xv = {xv}")
+    print(f"Yv = {yv}")
+
+if __name__ == "__main__":
+    calcular_vertice()

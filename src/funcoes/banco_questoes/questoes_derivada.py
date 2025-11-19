e2cb6b1b4ec269739744f29b0d156579cb750d78
Vinicius Antonino <gabrielantonino2004@gmail.com>
2025-11-19 16:43:35 -0300

    :sparkles: feat: Adição de arquivos com questões de derivada e vértice do banco de questões

---
+++ src/funcoes/banco_questoes/questoes_derivada.py
@@ -0,0 +1,20 @@
+questoes_derivada = [
+    {
+        "titulo": "Derivada de f(x) = 3x² - 2x + 7",
+        "enunciado": "Calcule a derivada da função f(x) = 3x² - 2x + 7.",
+        "resposta": "f'(x) = 6x - 2",
+        "passo_a_passo": (
+            "Aplicando a regra da potência:\n"
+            "d/dx(3x²) = 3*2x = 6x\n"
+            "d/dx(-2x) = -2\n"
+            "d/dx(7) = 0\n"
+            "f'(x) = 6x - 2"
+        ),
+    },
+    {
+        "titulo": "Derivada de f(x) = 5x³ + 10",
+        "enunciado": "Calcule a derivada de f(x) = 5x³ + 10.",
+        "resposta": "f'(x) = 15x²",
+        "passo_a_passo": ("d/dx(5x³) = 5*3x² = 15x²\n" "d/dx(10) = 0\n" "f'(x) = 15x²"),
+    },
+]
---
+++ src/funcoes/banco_questoes/questoes_vertice.py
@@ -0,0 +1,22 @@
+questoes_vertice = [
+    {
+        "titulo": "Calcular o vértice de f(x) = x² - 4x + 1",
+        "enunciado": "Encontre o vértice da função f(x) = x² - 4x + 1.",
+        "resposta": "V = (2, -3)",
+        "passo_a_passo": (
+            "Xv = -b / (2a)\n"
+            "Xv = -(-4) / (2*1) = 4/2 = 2\n"
+            "Yv = f(2) = (2)² - 4(2) + 1 = 4 - 8 + 1 = -3"
+        ),
+    },
+    {
+        "titulo": "Vértice de função invertida",
+        "enunciado": "Calcule o vértice da função f(x) = -3x² + 6x + 2.",
+        "resposta": "V = (1, 5)",
+        "passo_a_passo": (
+            "Xv = -b / (2a)\n"
+            "Xv = -6 / (2 * -3) = 1\n"
+            "Yv = f(1) = -3(1)² + 6(1) + 2 = -3 + 6 + 2 = 5"
+        ),
+    },
+]

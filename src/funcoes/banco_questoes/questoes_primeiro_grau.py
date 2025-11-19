af3d6dee1f966ee74ecf01a5bca6e8c6abafc0c5
Vinicius Antonino <gabrielantonino2004@gmail.com>
2025-11-19 16:42:00 -0300

    :sparkles: feat: Adição de arquivos com questões de função de primeiro e segundo grau do banco de questões

---
+++ src/funcoes/banco_questoes/questoes_primeiro_grau.py
@@ -0,0 +1,16 @@
+questoes_primeiro_grau = [
+    {
+        "titulo": "Calcular a raiz de 2x + 8 = 0",
+        "enunciado": "Resolva a equação: 2x + 8 = 0.",
+        "resposta": "x = -4",
+        "passo_a_passo": ("2x + 8 = 0\n" "2x = -8\n" "x = -8 / 2\n" "x = -4"),
+    },
+    {
+        "titulo": "Calcular o valor de f(3) em f(x) = 4x - 5",
+        "enunciado": "Determine f(3) para a função f(x) = 4x - 5.",
+        "resposta": "f(3) = 7",
+        "passo_a_passo": (
+            "f(x) = 4x - 5\n" "Substituindo x = 3:\n" "f(3) = 4(3) - 5 = 12 - 5 = 7"
+        ),
+    },
+]
---
+++ src/funcoes/banco_questoes/questoes_segundo_grau.py
@@ -0,0 +1,22 @@
+questoes_segundo_grau = [
+    {
+        "titulo": "Calcular raízes de  x² - 5x + 6",
+        "enunciado": "Encontre as raízes da função f(x) = x² - 5x + 6.",
+        "resposta": "x₁ = 3, x₂ = 2",
+        "passo_a_passo": (
+            "Δ = b² - 4ac\n"
+            "Δ = (-5)² - 4*1*6 = 25 - 24 = 1\n"
+            "x₁ = (5 + 1) / 2 = 3\n"
+            "x₂ = (5 - 1) / 2 = 2"
+        ),
+    },
+    {
+        "titulo": "Tipo de abertura e Delta",
+        "enunciado": "Determine o valor do Delta e o tipo de abertura da parábola f(x) = 2x² - 4x + 1.",
+        "resposta": "Δ = 8 ; parábola aberta para cima",
+        "passo_a_passo": (
+            "Δ = (-4)² - 4 * 2 * 1 = 16 - 8 = 8\n"
+            "Como a = 2 > 0, a parábola é aberta para cima."
+        ),
+    },
+]
